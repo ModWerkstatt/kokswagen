c58621f571259e7a4f874c6591aacf52981ca97a
Florian Thiel <flth@flth.de>
2024-08-25 14:55:12 +0200

    initial

---
+++ strings.lua
@@ -0,0 +1,34 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "DB Coke cars (Fb-zz 132 / Fbs-zz 131)",
+		["mod_desc"] = "The German Federal Railways procured 80 bucket wagons especially for coke handling in 1957 and classified them as Okmm 58. The two-part buckets stand on the longitudinal girders in such a way that they remain closed by their own weight. To empty them, the crane gear hooked to the side brackets is lifted, thus opening the bucket shells. The wagons were mostly used in block trains.",
+
+        ["Kokswagen_menu"] = "Coke cars (Fb-zz 132 / Fbs-zz 131)",
+		["Fbs-zz131_name"] = "Fbs-zz 131 (2001)",
+        ["Fbs-zz131_desc"] = "To speed up special coke transports, where Fb-zz 132 were still used, the Paderborn works developed a new bucket car in 2001. 10 units were built and used in block trains together with the reworked Fb-zz 132.",
+        ["Okmm58_name"] = "Okmm 58 (1958)",
+		["Okmm58_desc"] = "The German Federal Railways procured 80 bucket wagons especially for coke handling in 1957 and classified them as Okmm 58. The two-part buckets stand on the longitudinal girders in such a way that they remain closed by their own weight. For emptying, the crane gear hooked to the side brackets is lifted and thus the bucket shells are opened.",
+        ["Fb-zz132_name"] = "Fb-zz 132 (1965)",
+        ["Fb-zz132_DBAG_name"] = "Fb-zz 132 DB AG (1994)",
+		["Fb-zz132_desc"] = "From 1965 onwards, the Okmm 58s were re-designated as Fb-zz 132s in accordance with RIV guidelines. They were used in mostly block trains until their period of use was reached in 2000. Some wagons are refurbished in the Paderborn factory and remain in service with the Fbs-zz 131 for a few more years.",
+
+	},
+	de = {
+
+		["mod_name"] = "DB Kokswagen (Fb-zz 132 / Fbs-zz 131)",
+		["mod_desc"] = "Speziell für den Koksumschlag beschafft die Deutsche Bundesbahn im Jahre 1957 80 Kübelwagen und reihte sie als Okmm 58 ein. Die zweiteiligen Kübel stehen so auf den Langträgern, dass sie durch ihr eigenes Gewicht geschlossen bleiben. Zum Entleeren wird das an den seitlichen Bügeln eingehakte Krangeschirr angehoben und somit die Kübelschalen geöffnet. Die Wagen wurden zumeist in Ganzzügen eingesetzt.",
+
+        ["Kokswagen_menu"] = "Kokswagen (Fb-zz 132 / Fbs-zz 131)",
+		["Fbs-zz131_name"] = "Fbs-zz 131 (2001)",
+        ["Fbs-zz131_desc"] = "Zur Beschleunigung spezieller Koksverkehre, wo noch Fb-zz 132 eingesetzt wurden, entwickelt das Werk Paderborn im Jahr 2001 einen neuen Kübelwagen. Es wurden 10 Stück gebaut und zusammen mit den überarbeiteten Fb-zz 132 in Ganzzügen eingesetzt.",
+        ["Okmm58_name"] = "Okmm 58 (1958)",
+		["Okmm58_desc"] = "Speziell für den Koksumschlag beschafft die Deutsche Bundesbahn im Jahre 1957 80 Kübelwagen und reihte sie als Okmm 58 ein. Die zweiteiligen Kübel stehen so auf den Langträgern, dass sie durch ihr eigenes Gewicht geschlossen bleiben. Zum Entleeren wird das an den seitlichen Bügeln eingehakte Krangeschirr angehoben und somit die Kübelschalen geöffnet.",
+        ["Fb-zz132_name"] = "Fb-zz 132 (1965)",
+        ["Fb-zz132_DBAG_name"] = "Fb-zz 132 DB AG (1994)",
+		["Fb-zz132_desc"] = "Ab 1965 wurden die Okmm 58 nach RIV Richtline zu Fb-zz 132 umgezeichnet. Sie wurden in meist Ganzzügen eingesetzt, bis ihre Nutzungzeit im Jahr 2000 erreicht ist. Einige Wagen werden im Werk Paderborn aufgearbeitet und bleiben mit den Fbs-zz 131 noch einige Jahre im Einsatz.",
+
+	},
+}
+end
